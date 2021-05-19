e058b2dff695943cc4ea9aea3cab363752ff9353
Takayuki-Oiwake <81731266+Takayuki-Oiwake@users.noreply.github.com>
2021-05-20 01:08:54 +0900

    課題提出
    js4章4-1

---
+++ JavaScript/4-1/main.js
@@ -0,0 +1,60 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        list: [],
+        addText: '',
+        search:'',
+    },
+    //watchでlistの変更を監視
+    watch: {
+        list: {
+            handler: function() {
+                //localStorageにデータを保存
+                localStorage.setItem("list", JSON.stringify(this.list));
+            },
+            deep: true
+        }
+    },
+    //マウントされた時にlocalStorageからデータを取得
+    mounted: function() {
+        this.list = JSON.parse(localStorage.getItem("list")) || [];
+    },
+
+    computed: {
+        // 問２
+        task: function() {
+            return this.list.filter(function(todo) {
+                return !todo.isChecked;
+            }).length+ '/' + this.list.length;
+        },
+        // 問３
+        list: function() {
+            var list = [];
+            for(var i in this.list){
+                var todo = this.list[i];
+                if(todo.text.indexOf(this.search)!==-1){
+                    list.push(todo);
+                }
+            }
+            return list;
+        },    
+        
+    },
+
+    methods: {
+        addToDo: function() {
+            if (this.addText !== '') {
+                this.list.push({
+                    text: this.addText, 
+                    isChecked: false,
+                });
+            }
+            this.addText = '';
+        },
+        deleteBtn: function() {
+            this.list = this.list.filter(function(todo) {
+                return !todo.isChecked;
+            });
+        }
+    }
+});
